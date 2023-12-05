fd35e7fa1815d39d6536954f972df9c68d25e1e8
Vicky2509 <nayabratswain@gmail.com>
2023-12-05 22:06:35 +0530

    mongo db added

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongo db!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
