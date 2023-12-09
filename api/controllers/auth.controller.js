2c66165412a7870a40b750f7f8f329f676b093da
Vicky2509 <nayabratswain@gmail.com>
2023-12-09 23:22:42 +0530

    signin page functionality added with ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
       validUser.password
     );
     if (!validPassword) return next(errorHandler(401, "Incorrect Password"));
-    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const {password: pass, ...rest} = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+const SignIn = () => {
+  const [formData, setformData] = useState({});
+  const [Error, setError] = useState(null);
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-const Signin = () => {
+  const handleChange = (e) => {
+    setformData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      } else {
+        setLoading(false);
+        setError(null);
+        navigate('/');
+      }
+    } catch (Error) {
+      setLoading(false);
+      setError(Error.message);
+    }
+  };
+
   return (
-<h1>bia</h1>
+    <div>
+      <h1 className="text-3xl my-3 font-semibold text-center p-3">Sign In</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex  max-w-lg flex-col mx-auto gap-3"
+      >
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="p-3 rounded-lg border-2 border-slate-500"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="p-3 rounded-lg border-2 border-slate-500"
+          onChange={handleChange}
+        />
+        <button
+          disabled={Loading}
+          className="bg-slate-700 p-3 text-lg text-white font-semibold rounded-lg uppercase hover:opacity-70"
+        >
+          {Loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+
+      <div className="flex gap-1 mt-2 justify-center">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      {Error && <h4 className="text-red-500 mt-5 text-center font-semibold">{Error}</h4>}
+    </div>
   );
 };
 
-export default Signin;
+export default SignIn;
